2135d53df01144486a682452aed6bc79e79fdbc4
Carmelo Sanchez <Sanccarm@mail.gvsu.edu>
2025-07-23 00:51:46 -0400

    forgot to save mainmenu.gd

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -9,7 +9,7 @@ func _ready():
 # Doing this for now, soon add a click any button functionality 
 func fade_in_menu_buttons():
 	var tween = create_tween()
-	tween.tween_property($VBoxContainer, "modulate:a", 1.0, 10.0)
+	tween.tween_property($VBoxContainer, "modulate:a", 1.0, 3.0)
 	await tween.finished
 	
 	
